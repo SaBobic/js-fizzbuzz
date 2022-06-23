6ad081a244f652d00de4ec03facff22de2265f4f
SaBobic <fabrizioettori@gmail.com>
2022-06-23 16:06:01 +0200

    fixed bonus script

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -11,21 +11,26 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 // 1. Definisco le variabili globali, in questo caso elementNumber (100 da traccia), elementText (il testo da stampare) e il main (dove stampare il testo)
 
-    // Variabili costanti
+// Variabili costanti
 const elementNumber = 100;
 const main = document.getElementById('main');
 
-    // Variabile da aggregare nei cicli
+// Variabile da aggregare nei cicli
 let elementText = '<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-6 g-3 mx-auto">';
 
 // 2. Creo un ciclo (100 giri) che stampi in console elementText
 for (let i = 1; i <= elementNumber; i++){
-    elementText += `<div class="col"><div class="my-card">${i}</div></div>`;
-
+    
     // 3. Aggiungo all'interno del ciclo le condizioni di elementText se i perfettamente divisibile per 3, 5 e 15
-    if (i % 3 === 0) elementText += '<div class="col"><div class="my-card fizz">Fizz</div></div>';
-    if (i % 5 === 0) elementText += '<div class="col"><div class="my-card buzz">Buzz</div></div>';
-    if (i % 15 === 0) elementText += '<div class="col"><div class="my-card fizzbuzz">FizzBuzz</div></div>';
+    if (i % 15 === 0) {
+        elementText += '<div class="col"><div class="my-card fizzbuzz">FizzBuzz</div></div>';
+    } else if (i % 3 === 0) {
+        elementText += '<div class="col"><div class="my-card fizz">Fizz</div></div>';
+    } else if (i % 5 === 0) {
+        elementText += '<div class="col"><div class="my-card buzz">Buzz</div></div>';
+    } else {
+        elementText += `<div class="col"><div class="my-card">${i}</div></div>`;
+    }
 }
 
 elementText += '</div>';
