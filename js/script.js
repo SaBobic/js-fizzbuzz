b25924e3fec5afd2f1667bc54685a1e4b78edc50
SaBobic <fabrizioettori@gmail.com>
2022-06-23 14:05:49 +0200

    added 1 to 100 cycle

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 /*
-1. 
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+1. Definisco le variabili globali, in questo caso elementNumber (100 da traccia) ed elementText (il testo da stampare)
+2. Creo un ciclo (100 giri) che stampi in console elementText
+3. Aggiungo all'interno del ciclo le condizioni di elementText se un numero è perfettamente divisibile per 3, 5 e 15
+
 */
+
+const elementNumber = 100;
+let elementText = "";
+
+for (let i = 1; i <= elementNumber; i++){
+    console.log(i);
+}
