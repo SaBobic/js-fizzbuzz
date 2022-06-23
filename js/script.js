78bf762db39609418c915767d5b00937cac4923c
SaBobic <fabrizioettori@gmail.com>
2022-06-23 14:17:23 +0200

    added variables for %3, %5, %15

--- js/script.js
+++ js/script.js
@@ -9,9 +9,17 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 */
 
+// 1. Definisco le variabili globali, in questo caso elementNumber (100 da traccia) ed elementText (il testo da stampare)
 const elementNumber = 100;
 let elementText = "";
 
+// 2. Creo un ciclo (100 giri) che stampi in console elementText
 for (let i = 1; i <= elementNumber; i++){
-    console.log(i);
+    elementText = i;
+
+    // 3. Aggiungo all'interno del ciclo le condizioni di elementText se un numero è perfettamente divisibile per 3, 5 e 15
+    if (i % 3 === 0) elementText = "Fizz";
+    if (i % 5 === 0) elementText = "Buzz";
+    if (i % 15 === 0) elementText = "FizzBuzz";
+    console.log(elementText);
 }
